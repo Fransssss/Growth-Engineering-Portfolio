6a27524890a8da1c698720b86bad649642665db8
Frans <97081479+Fransssss@users.noreply.github.com>
2026-03-22 02:33:24 -0700

    Update funnel analysis.sql

--- funnel_analysis.sql
+++ funnel_analysis.sql
@@ -1,9 +1,19 @@
--- funnel analysis: number of users drop off at each stage
-SELECT event_name, COUNT(DISTINCT uesr_id) AS user_count
+-- Funnel analysis: number of users drop off at each stage
+SELECT event_name, COUNT(DISTINCT user_id) AS user_count
 FROM user_events
 WHERE event_name IN ('signed_up', 'sent_message', 'upgraded_to_pro')
 GROUP BY event_name;
 
 -- Users who never sent a message
+SELECT u.name, e.event_name,
+FROM users u
+LEFT JOIN user_events e 
+ON u.user_id = e.user_id 
+WHERE e.event_name is NULL;
 
 -- Event sequence for each user
+SELECT user_id, event_name, created_at,
+ROW_NUMBER() OVER (PARTITION BY user_id 
+ORDER BY created_at)
+AS event_order
+FROM user_events;
