a63eab0e63b1dd08325b71e4990d30fbde382760
Frans <97081479+Fransssss@users.noreply.github.com>
2026-03-23 12:54:44 -0700

    p-value & statistical significance

--- ab_test.py
+++ ab_test.py
@@ -1,25 +1,47 @@
-import pandas as pd
+import pandas as pd 
+from scipy import stats
 
-# syntetic dataset of users in A/B test
+# dataset
 data = {
-    'user_id': range(1,11),
-    'group': ['control','control','control','control','control',
-              'treatment','treatment','treatment','treatment','treatment'],
-              'activated': [0,1,0,1,0,1,1,1,0,1],
-              'country': ['US','US','UK','US','CA','US','UK','US','CA','US']
+    'user_id': range(1,101),
+    'group':['control'] * 50 + ['treatment'] * 50,
+    'activated' : [0,1,0,1,0,1,0,0,1,0,1,0,1,0,0,1,0,1,0,0,
+                  1,0,1,0,1,0,0,1,0,1,0,1,0,0,1,0,1,0,1,0,
+                  0,1,0,1,0,0,1,0,1,0,
+                  1,1,0,1,1,1,0,1,1,1,1,0,1,1,1,0,1,1,0,1,
+                  1,1,0,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,
+                  0,1,1,1,0,1,1,1,1,0]
 }
 
 df = pd.DataFrame(data)
 
-# view dataframe
-print("Dataframe:")
-print(df)
+# activation rate per group
+activation_rates = df.groupby('group')['activated'].mean()
+print("\nActivation Rates:")
+print(activation_rates)
 
-# filter for treatment group only
-treatment = df[df['group'] == 'treatment']
-print("\nTreatment Group:")
-print(treatment)
+# split into control and treatment 
+control = df[df['group'] == 'control']['activated']
+treatment = df[df['group'] == 'treatment']['activated']
 
-# basic stats
-print("\nTreatment Group Stats:")
-print(treatment.describe())
+# run a two-proportion z-test
+control_activated = control.sum()
+treatment_activated = treatment.sum() 
+control_total = len(control)
+treatmnet_total = len(treatment)
+
+# chi-square test
+contingency_table = pd.crosstab(df['group'],df['activated'])
+print("\nContingency table:")
+print(contingency_table)
+
+chi2, p_value, dof, expected = stats.chi2_contingency(contingency_table)
+print(f"\nChi-square statistic: {chi2:.4f}")
+print(f"p-value: {p_value:.4f}\n")
+
+if p_value < 0.05:
+    print("Result: Statistically significant")
+else:
+    print("Result: Not statistically significant")
+
+print('\n')
